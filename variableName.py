7a2ea167ee37a932a773ce30b8776f62c5f321d9
Geneilson Marques <geneilson@gmail.com>
2020-09-15 06:17:54 +0100

    Update VariableName.py
    Update file

--- variableName.py
+++ variableName.py
@@ -2,24 +2,21 @@
 import re
 def variableName(name):
     isValidVariable = True
-    containNumbers = re.findall('[0-9]', name[0])
     containLetters = re.findall('[a-zA-Z]', name[0])
-    containUnderscore = re.findall('_', name)
-    containTrace = re.findall('-', name)
+    containUnderscore = re.findall('_', name[0])
     containSpace = re.findall('\s', name)
-    print(containSpace)
-    # otherChars = re.findall('[^a-zA-Z][^0-9][^_]', name)
-    otherChars = re.search('([^a-zA-Z][^0-9])', name)
-    if not(containLetters) or not(containUnderscore):
+    otherChars = re.findall('([^a-zA-Z0-9_])', name)
+    if not(containLetters) and not(containUnderscore):
         isValidVariable = False
         return isValidVariable
     elif containSpace:
         isValidVariable = False
         return isValidVariable
-    
-
+    elif otherChars:
+        isValidVariable = False
+        return isValidVariable
     return isValidVariable
 
-name = "va [riab_le-0"
+name = "0ss"
 result = variableName(name)
 print(result)

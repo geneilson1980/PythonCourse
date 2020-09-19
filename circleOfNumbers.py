6a545305aede3e7387cf56bcfff77d511a0fcfb4
Geneilson Marques <geneilson@gmail.com>
2020-09-19 09:41:35 +0100

    Update CircleOfNumbers.py
    Update CircleOfNumbers.py

--- circleOfNumbers.py
+++ circleOfNumbers.py
@@ -1,21 +1,21 @@
 def circleOfNumbers(n, firstNumber):
     expectedOutput = ''
     refDictionary = {4: 2, 6: 3, 8: 4, 10: 5, 12: 6, 14: 7, 16: 8, 18: 9, 20: 10}
+    refList = []
+    for i in range(n):
+        refList.append(i)
     refNumber = refDictionary[n]
-    i = firstNumber+1
+    refIndexFNumber = refList.index(firstNumber)
+    i = firstNumber
     cicle = 0
     count = 1
-    while i <= n-1:
+    while True:
         if count == refNumber:
-            if cicle == 0:
-                expectedOutput = i
-                return expectedOutput
-            else:
-                expectedOutput = i -1
-                return expectedOutput
+            expectedOutput = i
+            return expectedOutput
         if i == n-1 and count != refNumber:
             i = 0
-            cicle += 1 
+            count += 1
             continue
         count += 1
         i += 1
@@ -23,8 +23,8 @@ def circleOfNumbers(n, firstNumber):
 
     #I have to develop the another part of the code, where the number should continue restarting from 0
 
-n = 4
-firstNumber = 1
+n = 6
+firstNumber = 3
 result = circleOfNumbers(n, firstNumber)
 print(result)
 
@@ -38,3 +38,18 @@ print(result)
 # null
 # Expected Output:
 # 1
+
+    # while i  < n:
+    #     if count == refNumber:
+    #         if cicle == 0:
+    #             expectedOutput = i + 1
+    #             return expectedOutput
+    #         else:
+    #             expectedOutput = i
+    #             return expectedOutput
+    #     if i == n-1 and count != refNumber:
+    #         i = 0
+    #         cicle += 1 
+    #         continue
+    #     count += 1
+    #     i += 1

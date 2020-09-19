8b79908c84032421227e80d21b66a4d0ab299fcf
Geneilson Marques <geneilson@gmail.com>
2020-09-19 19:25:41 +0100

    Update CircleOfNumbers.py
    Update CircleOfNumbers.py

--- circleOfNumbers.py
+++ circleOfNumbers.py
@@ -2,10 +2,14 @@ def circleOfNumbers(n, firstNumber):
     expectedOutput = ''
     refDictionary = {4: 2, 6: 3, 8: 4, 10: 5, 12: 6, 14: 7, 16: 8, 18: 9, 20: 10}
     refNumber = refDictionary[n]
-    if firstNumber + refNumber <= n:
-        expectedOutput = firstNumber + refNumber
+    if firstNumber + refNumber < n:
+        if firstNumber + refNumber < n:
+            expectedOutput = firstNumber + refNumber
+            return expectedOutput
+    elif n / 2 == firstNumber:
+        expectedOutput = 0
         return expectedOutput
-    else:
+    else:        
         i = firstNumber
         count = 1
         while i <= n -1:
@@ -18,7 +22,9 @@ def circleOfNumbers(n, firstNumber):
             count += 1
             i += 1
 
-n = 10
-firstNumber = 7
+n = 6
+firstNumber = 3
 result = circleOfNumbers(n, firstNumber)
 print(result)
+
+#test 4

b28e893cbf3e7ae5c7b3a4b36b32bdabecb75a92
Geneilson Marques <geneilson@gmail.com>
2020-09-28 05:55:09 +0100

    Update stringsRearrangement file
    Update stringsRearrangement file

--- stringsRearrangement.py
+++ stringsRearrangement.py
@@ -1,12 +1,28 @@
+import difflib
+
 def stringsRearrangement(inputArray):
     expectectResult = False
-    tempList = [[inputArray]]
+    tempList = [inputArray]
     x = len(inputArray)
-    for i in range(1, len(inputArray)-1):
-        tempList.append([inputArray[i-1]])
-        for j in range(i+2, len(inputArray)+1):
-            tempList.append([[inputArray[i-1]]][inputArray[j-1]])
-        tempList.append(inputArray[i])
+    i = 0
+    count = 0
+    while i < len(inputArray):
+        a = inputArray[i]
+        b = inputArray[i+1]
+        c = a[0]
+        for k in len(a):
+            if a[k] != b[k]:
+                count += 1
+
+
+        # if i == len(inputArray)-1:
+        #     i = 0
+        # #     continue
+
+        # j = 2
+        # tempList[i].append(inputArray[j-1])
+        i += 1
+
 
 
     return expectectResult

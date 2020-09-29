0001fcdbf990e14bed4cd47a785bb2ae57e68d52
Geneilson Marques <geneilson@gmail.com>
2020-09-29 06:05:58 +0100

    Update stringsRearrangement.py
    Update stringsRearrangement.py

--- stringsRearrangement.py
+++ stringsRearrangement.py
@@ -1,24 +1,19 @@
 from itertools import permutations
 import functools
-
 def stringsRearrangement(inputArray):
-    expectectResult = True
+    expectectResult = False
     newList = list(permutations(inputArray))
-    x = len(newList)
-    y = len(newList[0])
-    # reduce = 0
     for i in range(len(newList)):
         for j in range(len(newList[0])-1):
             a = newList[i][j]
             b = newList[i][j+1]
             c = functools.reduce(lambda x, y: x + 1 if y[0] != y[1] else x, zip(a, b), 0)
-            if c > 1:
-                expectectResult = False
+            if c != 1:
                 break
-        if c == 0 or c == 1:
+        if c == 1:
             expectectResult = True
             return expectectResult
-    return expectectResult
+    return expectectResult   
 
 inputArray = ["abc", "abx", "axx", "abc"]
 result = stringsRearrangement(inputArray)

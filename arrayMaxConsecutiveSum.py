4546306cd4cfb640f1ac30dff646ec5c9576ddde
Geneilson Marques <geneilson@gmail.com>
2020-10-02 06:41:38 +0100

    Update arrayMaxConsecutiveSum.py
    Update arrayMaxConsecutiveSum.py

--- arrayMaxConsecutiveSum.py
+++ arrayMaxConsecutiveSum.py
@@ -1,82 +1,28 @@
 def arrayMaxConsecutiveSum(inputArray, k):
-    newInput = inputArray.copy()
     expectedOutput = 0
     sumTemp = 0
-    factor = 0
-    refValue = 0
     listSize = len(inputArray)
-    for i in range(listSize):
-        resultSum = sumValues(i, k, inputArray)
-        if resultSum <= refValue:
-            if i + k >= listSize:
-                return expectedOutput
-            else:
-                continue
-        else:
-            refValue = resultSum
-        if i + k >= listSize:
-            if resultSum > expectedOutput:
-                expectedOutput = resultSum    
-                return expectedOutput
-            else:
-                return expectedOutput
-        elif resultSum > expectedOutput:
-            expectedOutput = resultSum
+    for i in range(k):
+        sumTemp += inputArray[i]
+    expectedOutput = sumTemp
+    for j in range(1, len(inputArray)-1):
+        sumTemp = sumTemp - inputArray[j-1]
+        x = inputArray[j+(k-1)]
+        sumTemp = sumTemp + inputArray[j+(k-1)]
+        if sumTemp > expectedOutput:
+            expectedOutput = sumTemp
+        z = (j + (k-1)) 
+        w = len(inputArray)-1
+        if (j + (k-1)) >= len(inputArray)-1:
+            return expectedOutput
+    return expectedOutput
 
-def sumValues(position, factor, list):
-    sumValuesList = 0
-    listSize = len(list)
-    countFactor = 0
-    for i in range(position, listSize):
-        sumValuesList += list[i]
-        countFactor += 1
-        if countFactor == factor:
-            return sumValuesList
-
-
-
-inputArray = [2, 3, 5, 1, 6]
-k = 2
+inputArray = [3, 2, 1, 1]
+k = 1
 result = arrayMaxConsecutiveSum(inputArray, k)
 print(result)
 
-
-# def fib_rapido(n):
-#     if n == 1 or n == 2:
-#         return 1
-#     f1 = 1
-#     f2 = 1
-#     for i in range(3, n + 1):
-#         f = f1 + f2
-#         f2 = f1
-#         f1 = f
-#     return f1
-
-# print(fib_rapido(7))
-
-
-
-
-# def arrayMaxConsecutiveSum(inputArray, k):
-#     newInput = inputArray.copy()
-#     expectedOutput = 0
-#     sumTemp = 0
-#     factor = 0
-#     for i in range(len(inputArray)):
-#         for j in range(k):
-#             x = newInput[j]
-#             sumTemp += newInput[j]
-#             factor += 1
-#             if sumTemp > expectedOutput and factor == k:
-#                 expectedOutput = sumTemp            
-#         newInput.remove(newInput[0])
-#         sumTemp = 0
-#         factor = 0
-#         if len(newInput) < k:
-#             return expectedOutput
-
-
-inputArray = [1, 3, 2, 4]
-k = 3
-result = arrayMaxConsecutiveSum(inputArray, k)
-print(result)
+# Free hint: imagine if your k = 1000, and length of array = 10000.
+# every new cycle you need to compute sum of 1000 elements.
+# but the difference between previous sum and current sum is just two components.
+# so, think how you can avoid naive summation of 1000 components

af6c8438f73b81594bb17a34e9f2313ce1809ee9
Geneilson Marques <geneilson@gmail.com>
2020-10-21 06:51:33 +0100

    Update bishopAndPawn.py
    Update bishopAndPawn.py

--- bishopAndPawn.py
+++ bishopAndPawn.py
@@ -1,40 +1,14 @@
 def bishopAndPawn(bishop, pawn):
     expectedOutput = False
-    # chestBoard = []
-    refHash = {'a': 0, 'b': 1, 'c': 2, 'd': 3, 'e': 4, 'f': 5, 'g': 6, 'h': 7}
-    refBishop = refHash[bishop[0]] 
-    refPawn = refHash[pawn[0]]
-    chestBoard = [[0]*8]*8
-    a = refHash[bishop[0]]
-    b = int(bishop[1])
-    # chestBoard[refHash[bishop[0]]][refHash[bishop[1-1]]] = bishop[1]
-    chestBoard[0][0] = 1
-
-
-
-    # for i in range(8):
-    #     column = []
-    #     for j in range(8):
-    #         column.append(0)
-    #     chestBoard.append(column)
+    refHash = {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'f': 6, 'g': 7, 'h': 8}
+    value1 = refHash[bishop[0]] - refHash[pawn[0]] 
+    value2 = int(bishop[1]) - int(pawn[1])
+    if abs(value1) == abs(value2):
+        expectedOutput = True
+        return expectedOutput
     return expectedOutput
 
-bishop = "a1"
-pawn = "c3"
+bishop = "e7"
+pawn = "a3"
 result = bishopAndPawn(bishop, pawn)
 print(result)
-
-
-# Creating a hash map to relate the bishop and pawn values to includ it in the chest board.
-
-
-# cinema =[
-#     [ 0, 0, 0, 0, 1 ],
-#     [ 0, 0, 0, 1, 1 ],
-#     [ 0, 0, 1, 1, 1 ],
-#     [ 0, 0, 0, 1, 1 ],
-#     [ 0, 0, 0, 0, 1 ]
-# ]
-
-# cinema[1][0] = 1
-# print(cinema)

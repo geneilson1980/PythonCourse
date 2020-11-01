5e1c84697eac7246f5b021b0cc017225dd2fb55b
Geneilson Marques <59176631+geneilson1980@users.noreply.github.com>
2020-11-01 10:39:21 +0000

    Update buildPalindrome.py
    Update buildPalindrome.py

--- buildPalindrome.py
+++ buildPalindrome.py
@@ -1,20 +1,17 @@
 def buildPalindrome(st):
+    i = 1
+    subString = st[:i]
+    if st == st[::-1]:
+        return st
     while True:
-        if st == st[::-1]:
-            return st
+        revertSubstring = subString[::-1]
+        sumString = st+revertSubstring
+        if (st+revertSubstring) == sumString[::-1]:
+            return st+revertSubstring
         else:
-            return st
-    # return expectedOutput
-    # APPROACH: get the string Base
-    # after, get the next char and check is a palindr, if not get the next 2 chars toghter and reverted and so son
+            i += 1
+            subString = st[0:i]
 
-st = "aba"
+st = "abcdc"
 result = buildPalindrome(st)
 print(result)
-
-
-
-
-# Just append the reverse of initial substrings of the string, from shortest to longest, to the string until you have a palindrome. e.g., for "acbab", try appending "a" which yields "acbaba", which is not a palindrome, then try appending "ac" reversed, yielding "acbabca" which is a palindrome.
-
-# Update: Note that you don't have to actually do the append. You know that the substring matches since you just reversed it. So all you have to do is check whether the remainder of the string is a palindrome, and if so append the reverse of the substring. Which is what Ptival wrote symbolically, so he should probably get the credit for the answer. Example: for "acbab", find the longest suffix that is a palindrome; that is "bab". Then append the remainder, "ac", in reverse: ac bab ca.

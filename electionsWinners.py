fe5222b814ddeac1ecdf43cc34a4f1693b9bfe84
Geneilson Marques <geneilson@gmail.com>
2020-11-02 06:48:46 +0000

    Update electionsWinners.py
    Update electionsWinners.py

--- electionsWinners.py
+++ electionsWinners.py
@@ -2,10 +2,9 @@ def electionsWinners(votes, k):
     votes.sort(reverse = True)
     expectedOutput = 0
     if votes[1] + k < votes[0]:
-            expectedOutput += 1
-            return expectedOutput
+        expectedOutput += 1
+        return expectedOutput
     for i in range(len(votes)):
-        a = votes[0]
         if votes[i] + k > votes[0]:
             expectedOutput += 1
         else:

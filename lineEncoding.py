4eb8604d45359481501e9e6187a6cfb9748b7252
Geneilson Marques <geneilson@gmail.com>
2020-11-04 09:26:19 +0000

    Update lineEncoding.py
    Update lineEncoding.py

--- lineEncoding.py
+++ lineEncoding.py
@@ -2,23 +2,35 @@ def lineEncoding(s):
     expectedOutput = ''
     uniqueChar = []
     countChar = 1
-    i = 0
-    while i < len(s):
+    x = len(s)
+    for i in range(len(s)-1):
         if s[i] == s[i+1]:
             countChar += 1
-            i += 1
-            if i == len(s)-1:
-                expectedOutput = expectedOutput + str(countChar) + s[i]
-            continue
-        else:
-            if countChar == 1:
-                expectedOutput = expectedOutput + s[i]
-                if i == len(s)-1:
-                    expectedOutput = expectedOutput + s[i+1]
-            else:
-                expectedOutput = expectedOutput + str(countChar) + s[i]
+        elif s[i] != s[i+1] and i == len(s)-1:
+            expectedOutput = expectedOutput + str(countChar) + s[i]
             countChar = 1
-            i += 1
+        else:
+            expectedOutput = expectedOutput + str(countChar) + s[i+1]
+            return expectedOutput
+   
+
+
+    # while i < len(s):
+    #     if s[i] == s[i+1]:
+    #         countChar += 1
+    #         i += 1
+    #         if i == len(s)-1:
+    #             expectedOutput = expectedOutput + str(countChar) + s[i]
+    #         continue
+    #     else:
+    #         if countChar == 1:
+    #             expectedOutput = expectedOutput + s[i]
+    #             if i == len(s)-1:
+    #                 expectedOutput = expectedOutput + s[i+1]
+    #         else:
+    #             expectedOutput = expectedOutput + str(countChar) + s[i]
+    #         countChar = 1
+    #         i += 1
     return expectedOutput
 
 s = "aabbbc"

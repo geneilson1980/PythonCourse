270bd95448b7ff46af2b5d9d8e2817f5cc2a714f
Geneilson Marques <geneilson@gmail.com>
2020-11-17 09:20:12 +0000

    Update chessKnight.py
    Update chessKnight.py

--- chessKnight.py
+++ chessKnight.py
@@ -1,24 +1,63 @@
 def chessKnight(cell):
-    chessBoard = [[1, 0, 1, 0, 1, 0, 1, 0,], [0, 1, 0, 1, 0, 1, 0, 1], 
-                  [1, 0, 1, 0, 1, 0, 1, 0,], [0, 1, 0, 1, 0, 1, 0, 1], 
-                  [1, 0, 1, 0, 1, 0, 1, 0,], [0, 1, 0, 1, 0, 1, 0, 1],
-                  [1, 0, 1, 0, 1, 0, 1, 0,], [0, 1, 0, 1, 0, 1, 0, 1]]
-    refChar = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6, 'H': 7}
+    refChar = {'a': 0, 'b': 1, 'c': 2, 'd': 3, 'e': 4, 'f': 5, 'g': 6, 'h': 7}
     expectedOutput = 0
-    pos1 = cell[0]
-    pos1a = int(cell[1])
-    getPos = refChar[pos1]
-    valueInChessBoard = chessBoard[getPos][pos1a-1]
-    if valueInChessBoard is True:
-        print('hello')
-    else:
-        print('no hello')
-    
-
+    horizontalPos = refChar[cell[0]]
+    verticalPos = 7 - (int(cell[1])-1)
+    if ((verticalPos+1 >= 0) and (verticalPos+1 < 8)) and ((horizontalPos-2 >= 0) and (horizontalPos-2 < 8))  :
+        expectedOutput += 1
+    if ((verticalPos-1 >= 0) and (verticalPos-1 < 8)) and ((horizontalPos-2 >= 0) and (horizontalPos-2 <8))  :
+        expectedOutput += 1
+    if ((verticalPos-2 >= 0) and (verticalPos-2 < 8)) and ((horizontalPos-1 >= 0) and (horizontalPos-1 < 8))  :
+        expectedOutput += 1
+    if ((verticalPos-2 >= 0) and (verticalPos-2 < 8)) and ((horizontalPos+1 >= 0) and (horizontalPos+1 < 8))  :
+        expectedOutput += 1
+    if ((verticalPos-1 >= 0) and (verticalPos-1 < 8)) and ((horizontalPos+2 >= 0) and (horizontalPos+2< 8))  :
+        expectedOutput += 1
+    if ((verticalPos+1 >= 0) and (verticalPos+1 < 8)) and ((horizontalPos+2 >= 0) and (horizontalPos+2 < 8))  :
+        expectedOutput += 1
+    if ((verticalPos+2 >= 0) and (verticalPos+2 < 8)) and ((horizontalPos+1 >= 0) and (horizontalPos+1 < 8))  :
+        expectedOutput += 1
+    if ((verticalPos+2 >= 0) and (verticalPos+2 < 8)) and ((horizontalPos-1 >= 0) and (horizontalPos-1 < 8))  :
+        expectedOutput += 1
     return expectedOutput
 
-
-
-cell = "A1"
+cell = "a3"
 result = chessKnight(cell)
 print(result)
+
+
+
+# def chessKnight(cell):
+#     chessBoard = [[1, 2, 3, 4, 5, 6, 7, 8,], [9, 10, 11, 12, 13, 14, 15, 16],
+#                   [17, 18, 19, 20, 21, 22, 23, 24,], [25, 26, 27, 28, 29, 30, 31, 32],
+#                   [33, 34, 35, 36, 37, 38, 39, 40,], [41, 42, 43, 44, 45, 46, 47, 48],
+#                   [49, 50, 51, 52, 53, 54, 55, 56,], [57, 58, 59, 60, 61, 62, 63, 64]]
+#     refChar = {'a': 0, 'b': 1, 'c': 2, 'd': 3, 'e': 4, 'f': 5, 'g': 6, 'h': 7}
+#     expectedOutput = 0
+#     horizontalPos = refChar[cell[0]]
+#     verticalPos = (len(chessBoard)-1) - (int(cell[1])-1)
+#     if ((verticalPos+1 >= 0) and (verticalPos+1 < len(chessBoard))) and ((horizontalPos-2 >= 0) and (horizontalPos-2 < len(chessBoard)))  :
+#         a = chessBoard[verticalPos+1][horizontalPos-2]
+#         expectedOutput += 1
+#     if ((verticalPos-1 >= 0) and (verticalPos-1 < len(chessBoard))) and ((horizontalPos-2 >= 0) and (horizontalPos-2 < len(chessBoard)))  :
+#         b = chessBoard[verticalPos-1][horizontalPos-2]
+#         expectedOutput += 1
+#     if ((verticalPos-2 >= 0) and (verticalPos-2 < len(chessBoard))) and ((horizontalPos-1 >= 0) and (horizontalPos-1 < len(chessBoard)))  :
+#         c = chessBoard[verticalPos-2][horizontalPos-1]
+#         expectedOutput += 1
+#     if ((verticalPos-2 >= 0) and (verticalPos-2 < len(chessBoard))) and ((horizontalPos+1 >= 0) and (horizontalPos+1 < len(chessBoard)))  :
+#         d = chessBoard[verticalPos-2][horizontalPos+1]
+#         expectedOutput += 1
+#     if ((verticalPos-1 >= 0) and (verticalPos-1 < len(chessBoard))) and ((horizontalPos+2 >= 0) and (horizontalPos+2< len(chessBoard)))  :
+#         e = chessBoard[verticalPos-1][horizontalPos+2]
+#         expectedOutput += 1
+#     if ((verticalPos+1 >= 0) and (verticalPos+1 < len(chessBoard))) and ((horizontalPos+2 >= 0) and (horizontalPos+2 < len(chessBoard)))  :
+#         f = chessBoard[verticalPos+1][horizontalPos+2]
+#         expectedOutput += 1
+#     if ((verticalPos+2 >= 0) and (verticalPos+2 < len(chessBoard))) and ((horizontalPos+1 >= 0) and (horizontalPos+1 < len(chessBoard)))  :
+#         g = chessBoard[verticalPos+2][horizontalPos+1]
+#         expectedOutput += 1
+#     if ((verticalPos+2 >= 0) and (verticalPos+2 < len(chessBoard))) and ((horizontalPos-1 >= 0) and (horizontalPos-1 < len(chessBoard)))  :
+#         h = chessBoard[verticalPos+2][verticalPos-1]
+#         expectedOutput += 1
+#     return expectedOutput

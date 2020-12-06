f7e680efbd0443864eb5ba9484675d033b60cc0d
Geneilson Marques <geneilson@gmail.com>
2020-12-06 08:27:02 +0000

    Update spiralNumbers.py
    Update spiralNumbers.py

--- spiralNumbers.py
+++ spiralNumbers.py
@@ -1,37 +1,47 @@
 def spiralNumbers(n):
-    expectedOutput = n
+    expectedOutput = [[None] * n] * n
     top = 0
-    down = len(n)-1
+    # down = len(n)-1
+    down = n-1
     left = 0
-    right = len(n)-1
+    # right = len(n)-1
+    right = n-1
     refNumber = 1
     dir = 0 # 0=Move left to right, 1=Move top to down, 2=Move right to left, 3=Move down to up
     while top <= down and left <= right:
         if dir == 0:
             for i in range(left, right+1):
-                print(expectedOutput[top][i], end=' ')
+                expectedOutput[top][i] = refNumber
+                # print(expectedOutput[top][i], end=' ')
+                refNumber += 1
             top += 1
-            print('')
+            # print('')
         elif dir == 1:
             for i in range(top, down+1):
-                print(expectedOutput[i][right], end=' ')
+                expectedOutput[i][right] = refNumber
+                # print(expectedOutput[i][right], end=' ')
+                refNumber += 1
             right -= 1
-            print('')
+            # print('')
         elif dir == 2:
             for i  in range(right, left-1, -1):
-                print(expectedOutput[down][i], end=' ')
+                expectedOutput[down][i] = refNumber
+                # print(expectedOutput[down][i], end=' ')
+                refNumber += 1
             down -= 1
-            print('')
+            # print('')
         elif dir == 3:
             for i  in range(down, top-1, -1):
-                print(expectedOutput[i][left], end=' ')
+                expectedOutput[i][left] = refNumber
+                # print(expectedOutput[i][left], end=' ')
+                refNumber += 1
             left += 1
         dir = (dir + 1) % 4
     return expectedOutput
 
 
-# n = 3
-n = [[1,2,3], [8,9,4], [7,6,5]]
+n = 3
+# n = [[1,2,3], [8,9,4], [7,6,5]]
 result = spiralNumbers(n)
 print(result)
 

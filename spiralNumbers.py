e5d1fda9580c2725c8022f971e7607319570e980
Geneilson Marques <geneilson@gmail.com>
2020-12-08 11:06:31 +0000

    Update spiralNumbers.py
    Update spiralNumbers.py

--- spiralNumbers.py
+++ spiralNumbers.py
@@ -1,51 +1,36 @@
 def spiralNumbers(n):
-    expectedOutput = [[None] * n] * n
+    newList = [[]* n for i in range(n)]
     top = 0
-    # down = len(n)-1
     down = n-1
     left = 0
-    # right = len(n)-1
     right = n-1
     refNumber = 1
     dir = 0 # 0=Move left to right, 1=Move top to down, 2=Move right to left, 3=Move down to up
     while top <= down and left <= right:
-        if dir == 0:
+        if dir == 0: #This if will populate the top line (horizontal)
             for i in range(left, right+1):
-                expectedOutput[top][i] = refNumber
-                # print(expectedOutput[top][i], end=' ')
+                newList[top].insert(i, refNumber)
                 refNumber += 1
             top += 1
-            # print('')
-        elif dir == 1:
+        elif dir == 1: #This elif will populate the last empty column (vertical)
             for i in range(top, down+1):
-                expectedOutput[i][right] = refNumber
-                # print(expectedOutput[i][right], end=' ')
+                newList[i].insert(left, refNumber)
                 refNumber += 1
             right -= 1
-            # print('')
-        elif dir == 2:
+        elif dir == 2: #This elif will populate the bottom line (horizontal)
             for i  in range(right, left-1, -1):
-                expectedOutput[down][i] = refNumber
-                # print(expectedOutput[down][i], end=' ')
+                newList[down].insert(left, refNumber)
                 refNumber += 1
             down -= 1
-            # print('')
-        elif dir == 3:
+        elif dir == 3: #This elif will populate the first column (vertical, down to top)
             for i  in range(down, top-1, -1):
-                expectedOutput[i][left] = refNumber
-                # print(expectedOutput[i][left], end=' ')
+                newList[i].insert(left, refNumber)
                 refNumber += 1
             left += 1
         dir = (dir + 1) % 4
-    return expectedOutput
+    return newList
 
 
-n = 3
-# n = [[1,2,3], [8,9,4], [7,6,5]]
+n = 5
 result = spiralNumbers(n)
 print(result)
-
-#NEXT STEPS
-# create new empty list
-# populate the list
-# populate last index with values.
